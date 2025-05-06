35979eda3a7410e5638042264eab082c93f6607d
Ronin-n <tangxl1124@outlook.com>
2025-05-06 13:10:23 +0800

    Update CAP_VFFGN_base.sh

--- scripts/VFFGN_base/CAP_VFFGN_base.sh
+++ scripts/VFFGN_base/CAP_VFFGN_base.sh
@@ -10,7 +10,7 @@ cmd="python train_VFFGN.py --dataset_mode=multimodal_miss --model=VFFGN_base
 --V_type=denseface --input_dim_v=342 --embd_size_v=128  --embd_method_v=maxpool
 --L_type=bert_large --input_dim_l=1024 --embd_size_l=128
 --AE_layers=256,128,64 --n_blocks=5 --num_thread=8 --corpus=IEMOCAP
---ce_weight=1.0 --mse_weight=8.0 --cl_weight=10 --consistent_weight=100
+--ce_weight=1.0 --cl_weight=10 --ist_weight=100
 --output_dim=4 --cls_layers=128,128 --dropout_rate=0.3
 --niter=46 --niter_decay=4 --verbose --print_freq=10
 --batch_size=16 --lr=2e-4 --run_idx=$run_idx --weight_decay=1e-5 --Is_part_miss=False --miss_rate=0.9
